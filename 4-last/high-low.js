81b8c386fb5f17061bb245b1a4fdf85281f0aa7a
Yoko Maruyama <maruyama@pxgrid.com>
2021-07-05 08:57:32 +0900

    デモアップ

---
+++ 4-last/high-low.js
@@ -0,0 +1,49 @@
+const answer = Math.floor( Math.random() * 11 );
+
+console.log("ボタンが押されたら回答者のボタンの値をもらう");
+
+// console.log("ボタンが押されたら回答者のボタンの値をもらう");
+// 宿題：ボタンを複数個おいて、それぞれのボタンがおされたときに、それぞれのメッセージを出力するようにする
+
+function comparison (answer , guessNumber){
+  if(guessNumber == answer){
+    return "あたり！";
+  }
+  else if (guessNumber < answer){
+    return "小さい";
+  }
+  else if (guessNumber > answer){
+    return "大きい";
+  }
+}
+
+// 画面に値の判定を表示させる
+function display (result){
+  const div = document.getElementById("result");
+  div.innerHTML = result;
+}
+
+const btn = document.querySelectorAll("button");
+btn.forEach((el) => {
+  el.addEventListener('click',  (event) => {
+    const guessNumber = event.currentTarget.value;
+    console.log(answer);
+    console.log(guessNumber);
+    const result = comparison (answer, guessNumber)
+    display (result);
+  });
+})
+
+// answerの初期値をリセットする方法がわからん。
+
+
+
+// console.log("判定する");
+
+// (answer, )
+
+// console.log("判定結果を受けて、表示する");
+
+// console.log("bingoだったらstart againボタンを表示する");
+
+// console.log("start againボタンが押されたら、はじめに戻る");
